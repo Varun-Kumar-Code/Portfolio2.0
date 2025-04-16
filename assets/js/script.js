f165fdc55beb42df4cf26a3b1852fd11d82f26d8
Varun Kumar <varunkumar1329@gmail.com>
2025-04-16 20:20:30 +0530

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -162,3 +162,23 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
+
+//Prevent righ click
+document.oncontextmenu = () => {
+    alert("Don't try right click")
+    return false
+}
+
+document.onkeydown = e => {
+    //Prevent F12 key
+    if (e.key == "F12") {
+        alert("Don't try to inspect element")
+        return false
+    }
+
+    //Prevent showing page source by ctrl + U
+    if (e.ctrlKey && e.key == "u") {
+        alert("Don't try to view page sources")
+        return false
+    }
+}
